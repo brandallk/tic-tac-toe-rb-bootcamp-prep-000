824b1ad596b09bc90cf8796e7245dac7526e0bb4
Benjamen King <brk.3rdof3kings@gmail.com>
2017-12-23 05:43:49 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,4 +51,14 @@ def turn(board)
   display_board(board)
 end
 
+def turn_count(board)
+  count = 0
+  for square in board
+    if ["X", "O"].include? square
+      count += 1
+    end
+  end
+  count
+end
+
 
